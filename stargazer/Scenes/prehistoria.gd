3106e55f2c97977a06547d1ca20749c26e10b720
Cocotrilo <nogalesisaac25@gmail.com>
2026-04-29 17:43:13 -0500

    with trial and error learnt to do a fade transition

--- stargazer/Scenes/prehistoria.gd
+++ stargazer/Scenes/prehistoria.gd
@@ -2,7 +2,13 @@ extends Node2D
 
 func _ready():
 	$Fire.fireON.connect(_on_FireON)
-
+	$CanvasLayer/ColorRect.modulate.a = 0.0
 func _on_FireON():
 	$Ground/ColorRect.color = Color(0.8, 0.3, 0.0)
+	fande_and_change()
+func fande_and_change():
+	var tween = create_tween()
+	tween.tween_property($CanvasLayer/ColorRect,"modulate:a", 10.0, 1.5)
 	
+	await tween.finished
+	get_tree().change_scene_to_file("res://Scenes/egypt.tscn")
